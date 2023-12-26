fbdf63903c6a76b251954d4240852fb224f0033a
Andrés Catalán <hola@andres.cat>
2023-12-26 00:19:10 -0500

    chore: update dependencies

--- src/commands/doc/generate/atlas.ts
+++ src/commands/doc/generate/atlas.ts
@@ -1,14 +1,14 @@
 import { dirname } from 'node:path';
 import { readdir } from 'node:fs/promises';
 import { fileURLToPath } from 'node:url';
-import { SfCommand, Flags } from '@salesforce/sf-plugins-core';
+import { SfCommand } from '@salesforce/sf-plugins-core';
 import { SfProject, Messages, NamedPackageDir } from '@salesforce/core';
 import { ExcelWriter } from '../../../shared/xlsx/ExcelWriter.js';
 
 import { Atlas } from '../../../shared/metadata/Atlas.js';
 
 Messages.importMessagesDirectory(dirname(fileURLToPath(import.meta.url)));
-const messages = Messages.loadMessages('plugin-documentation-atlas', 'doc.generate.atlas');
+const messages = Messages.loadMessages('plugin-atlas', 'doc.generate.atlas');
 
 export type DocGenerateAtlasResult = {
   path: string;
@@ -19,14 +19,14 @@ export default class DocGenerateAtlas extends SfCommand<DocGenerateAtlasResult> 
   public static readonly description = messages.getMessage('description');
   public static readonly examples = messages.getMessages('examples');
 
-  public static readonly flags = {
-    name: Flags.string({
-      summary: messages.getMessage('flags.name.summary'),
-      description: messages.getMessage('flags.name.description'),
-      char: 'n',
-      required: false,
-    }),
-  };
+  // public static readonly flags = {
+  //   name: Flags.string({
+  //     summary: messages.getMessage('flags.name.summary'),
+  //     description: messages.getMessage('flags.name.description'),
+  //     char: 'n',
+  //     required: false,
+  //   }),
+  // };
 
   // TODO - add flags and remove this exception
   // eslint-disable-next-line class-methods-use-this
